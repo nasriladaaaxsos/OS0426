1fc196db348728867094cf3a85f3beb975c3b699
muraddavid97-eng <muraddavid97@gmail.com>
2026-06-03 14:36:37 +0300

    login

--- myproject/app1/urls.py
+++ myproject/app1/urls.py
@@ -1,12 +1,17 @@
-from django.urls import path     
+from django.urls import path
 from . import views
 
-urlpatterns = [ 
-    path('', views.index),
-    path('create', views.create_appointment),
-    path('edit', views.edit_appointment),
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('login', views.login, name='login'),
+    path('create', views.create_appointment, name='create_appointment'),
+    path('edit', views.edit_appointment, name='edit_appointment'),
     path('viewall', views.view_all_appointment, name='showall'),
-    path('delete', views.delete_appointment),
-    path('viewupcomming', views.view_upcoming_appointment),
-    path('details/<int:id>', views.appointment_detail),
-    ]
+    path('delete', views.delete_appointment, name='delete_appointment'),
+    path('viewupcomming', views.view_upcoming_appointment, name='view_upcoming'),
+    path('details/<int:id>', views.appointment_detail, name='appointment_detail'),
+]
+
+
+
+
--- myproject/app1/views.py
+++ myproject/app1/views.py
@@ -1,32 +1,49 @@
-from django.shortcuts import render, redirect, HttpResponse
-from .models import *
-#Get
-#Landing page for login
+from django.shortcuts import render, redirect
+from .models import User
+
 def index(request):
     return render(request, 'index.html')
 
-#Get
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = User.objects.filter(username=username, password=password).first()
+        if user:
+            request.session['user_id'] = user.id
+            request.session['username'] = user.username
+            return redirect('showall')
+
+        return render(request, 'index.html', {
+            'error': 'Wrong username or password'
+        })
+
+    return redirect('home')
+
+
 def view_all_appointment(request):
     return render(request, 'viewappointments.html')
 
-#Get
+
 def create_appointment(request):
     return render(request, 'createappointment.html')
 
-#Get
+
 def edit_appointment(request):
     if request.method == "POST":
         edit_data(request.POST, request.POST['appointment_id'])
         return redirect('/edit')
     return render(request, 'editappointment.html')
 
-#Post request
+
 def delete_appointment(request):
     return redirect('showall')
+
 
 def view_upcoming_appointment(request):
     return render(request, 'viewupappointments.html')
 
+
 def appointment_detail(request, id):
     return render(request, 'detail.html')
-
