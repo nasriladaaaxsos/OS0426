4242ed1fcc75edafb6efafea7046dce7b4c3a8f9
Hosni2005 <hosninouraldin@gmail.com>
2026-06-04 13:27:07 +0300

    project-Hosni

--- myproject/app1/models.py
+++ myproject/app1/models.py
@@ -1,9 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    dateOfBirth = models.DateField()
+    created_At = models.DateTimeField(auto_now_add=True)
+    updated_At = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.username
 #Class User
 # Class attributes: username, password, dateOfBirth, created_At, updated_At
 
+
+
 #Class Appointment 
 # Class attributes: Appointment_Desc, Appointment_Date, created_At, updated_At
+class Appointment(models.Model) :
+    Appointment_Desc = models.TextField()
+    Appointment_Date = models.DateField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_At = models.DateTimeField(auto_now_add=True)
+
+
+
+def edit_data(data,id):
+    appointment = Appointment.objects.get(id = id )
+    appointment.desc =data['desc']
+    appointment.date = data['date']
+    appointment.save()
--- myproject/app1/views.py
+++ myproject/app1/views.py
@@ -4,7 +4,23 @@ from . import models
 #Get
 #Landing page for login
 def index(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = models.User.objects.filter(username=username, password=password).first()
+        if user:
+            request.session['user_id'] = user.id
+            request.session['username'] = user.username
+            return redirect('showall')
+
+        return render(request, 'index.html', {
+            'error': 'Wrong username or password'
+        })
+
+    
     return render(request, 'index.html')
+
+
 
 #Get
 def view_all_appointment(request):
@@ -12,10 +28,22 @@ def view_all_appointment(request):
 
 #Get
 def create_appointment(request):
-    return render(request, 'createappointment.html')
+    if request.method == 'POST' :
+        Appointment_Desc = request.POST['Appointment_Desc']
+        Appointment_Date = request.POST['Appointment_Date']
+        models.Appointment.objects.create(
+            Appointment_Desc=Appointment_Desc,
+            Appointment_Date=Appointment_Date
+        )
+        return redirect('/')
+    else :
+        return render(request, 'createappointment.html')
 
 #Get
 def edit_appointment(request):
+    if request.method == "POST":
+        models.edit_data(request.POST, request.POST['appointment_id'])
+        return redirect('/edit')
     return render(request, 'editappointment.html')
 
 #Post request
@@ -25,6 +53,11 @@ def delete_appointment(request):
 def view_upcoming_appointment(request):
     return render(request, 'viewupappointments.html')
 
+
 def appointment_detail(request, id):
-    return render(request, 'detail.html')
+    appointment = models.Appointment.objects.get(id = id)
+    context = {
+        "appointment" : appointment
+    }
+    return render(request, 'detail.html',context)
 
